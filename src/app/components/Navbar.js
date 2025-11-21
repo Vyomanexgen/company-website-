dac0c3321f7f992da3e24ba7c39f0d36672bedf9
sanath2567 <sriramsanath555@gmail.com>
2025-11-21 19:04:26 +0530

    dd

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -437,7 +437,7 @@ export default function Navbar() {
                     `}
                   ></span>
                 </Link>
-              </li>
+              </li>   
             ))}
           </ul>
 
